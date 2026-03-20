cb0587267b13405ad13e3a1bd814e99a1059411d
Manurobate <contact@robate.fr>
2026-03-20 13:47:22 +0100

    Ajout des subject

---
+++ src/subject/bdd.ts
@@ -0,0 +1,14 @@
+export const SUBJECTS = [
+  {
+    id: 1,
+    name: 'Francais',
+  },
+  {
+    id: 2,
+    name: 'Anglais',
+  },
+  {
+    id: 3,
+    name: 'Mathématiques',
+  },
+];
---
+++ src/subject/subject.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import type { InterfacePostSubject, InterfaceSubject } from './subject';
+import { SUBJECTS } from './bdd';
+import { SubjectService } from './subject.service';
+
+@Controller('subject')
+export class SubjectController {
+  constructor(private readonly subjectService: SubjectService) {}
+
+  @Get()
+  findAll(): InterfaceSubject[] {
+    return SUBJECTS;
+  }
+
+  @Get(':id')
+  findOneById(@Param('id') id: string): InterfaceSubject {
+    return this.subjectService.findOneById(+id); // cast en number
+  }
+
+  @Post()
+  addSubject(@Body() subject: InterfacePostSubject): InterfaceSubject[] {
+    return this.subjectService.createNewSubject(subject);
+  }
+}
