a7e9cf7b3a7f9bb30bef1d2f93332c336e9a0268
Manurobate <contact@robate.fr>
2026-03-25 11:52:13 +0100

    build(bdd) : ajout du premier fichier de migration pour initialiser la BDD

---
+++ src/database/migrations/1774434618049-initialisationBdd.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialisationBdd1774434618049 implements MigrationInterface {
+    name = 'InitialisationBdd1774434618049'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`level_entity\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`subject_entity\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`levelId\` int NULL, UNIQUE INDEX \`REL_1380fa88fa7bb134c30d8c083b\` (\`levelId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`ALTER TABLE \`subject_entity\` ADD CONSTRAINT \`FK_1380fa88fa7bb134c30d8c083b9\` FOREIGN KEY (\`levelId\`) REFERENCES \`level_entity\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`subject_entity\` DROP FOREIGN KEY \`FK_1380fa88fa7bb134c30d8c083b9\``);
+        await queryRunner.query(`DROP INDEX \`REL_1380fa88fa7bb134c30d8c083b\` ON \`subject_entity\``);
+        await queryRunner.query(`DROP TABLE \`subject_entity\``);
+        await queryRunner.query(`DROP TABLE \`level_entity\``);
+    }
+
+}
